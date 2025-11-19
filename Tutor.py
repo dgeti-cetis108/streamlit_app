741662ec0760951cfd67e95bcefec0eb0cc3a451
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-11-19 12:57:48 -0700

    Definir la clase Tutor con enumeraciones

--- Tutor.py
+++ Tutor.py
@@ -8,4 +8,26 @@
 
 # nombre, primer apellido, segundo apellido, ine,
 # parentesco, comprobante de domicilio, ocupación,
-# discapacidad
+# discapacidad, teléfono1, teléfono2, correo electrónico
+
+from enum import Enum
+
+class Parentesco(Enum):
+    Padre = 1
+    Madre = 2
+    Abuelo = 3
+    Hermano = 4
+    Tio = 5
+    Encargado = 6
+
+class Tutor:
+    nombre: str
+    primer_apellido: str
+    segundo_apellido: str
+    ine: str # "images/tutores/20260115_tutor_ine.jpg"
+    parentesco: Parentesco
+    comprobante_domicilio: str
+    ocupacion: str
+    discapacidad: bool
+    telefono: str
+    correo_electronico: str
